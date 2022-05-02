bff214f6272153220c1305a25179681c031b776c
s.patil <s.patil@fz-juelich.de>
2022-05-02 16:24:58 +0200

    example python file

---
+++ data_access.py
@@ -0,0 +1,33 @@
+#load required libraries 
+import os
+import json
+import requests
+from pandas import json_normalize
+
+from dotenv import load_dotenv, find_dotenv
+
+#load environment variables 
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+#get the secret API key 
+SECRET_API_KEY = os.environ.get("SECRET_API_KEY")
+
+#send API access request 
+resolution = "NUTS3"
+region = "DEA29"
+type = "data"
+
+request_url = f"http://data.localised-project.eu/api/v1/{resolution}/?api_key={SECRET_API_KEY}&region={region}&type={type}"
+response = requests.get(request_url)
+
+#get the json response 
+response_json = response.json()
+
+#save the entire json response as a text file 
+with open('data.json', 'w') as f:
+    json.dump(response_json, f)
+
+#alternatively, save only the regional data (region_data) as a .csv file 
+data_df = json_normalize(response_json.get('regions')[0].get('region_data'))
+data_df.to_csv('region_data.csv')
