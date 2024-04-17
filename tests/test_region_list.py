9cb59b0ef9d3398c72f142652ccd493caa21faa9
s.pati <s.patil@fz-juelich.de>
2024-04-17 18:04:41 +0200

    update test

--- tests/test_region_list.py
+++ tests/test_region_list.py
@@ -7,7 +7,7 @@ from zoomin_client import client
 @pytest.mark.parametrize("spatial_resolution", ["NUTS1", "NUTS2", "NUTS3"])
 def test_get_region_list(api_key, spatial_resolution):
     """Check if region list is returned."""
-    output = client.get_region_list(api_key, spatial_resolution, country_code="DE")
+    output = client.get_region_metadata(api_key, spatial_resolution, country_code="DE")
     assert output[0].get("resolution") == spatial_resolution
 
 
@@ -15,7 +15,7 @@ def test_get_region_list(api_key, spatial_resolution):
 def test_save_regions(api_key, result_format):
     """Check if the results of get_regions() are saved properly."""
     save_path = os.path.join(os.path.dirname(__file__))
-    output = client.get_region_list(
+    output = client.get_region_metadata(
         api_key,
         spatial_resolution="NUTS3",
         country_code="DE",
