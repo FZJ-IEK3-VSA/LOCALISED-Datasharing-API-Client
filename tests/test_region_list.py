5cbc6e77fea0e8319fb5020cec1a778d6d98e9a7
s.pati <s.patil@fz-juelich.de>
2024-04-17 18:08:21 +0200

    update test

--- tests/test_region_list.py
+++ tests/test_region_list.py
@@ -11,30 +11,21 @@ def test_get_region_list(api_key, spatial_resolution):
     assert output[0].get("resolution") == spatial_resolution
 
 
-@pytest.mark.parametrize("result_format", ["json", "df"])
-def test_save_regions(api_key, result_format):
+
+def test_save_regions(api_key):
     """Check if the results of get_regions() are saved properly."""
     save_path = os.path.join(os.path.dirname(__file__))
     output = client.get_region_metadata(
         api_key,
         spatial_resolution="NUTS3",
         country_code="DE",
-        result_format=result_format,
         save_result=True,
         save_path=save_path,
     )
 
-    if result_format == "json":
-        assert isinstance(output, list)
-        assert len(output) == 401
+    assert isinstance(output, list)
+    assert len(output) == 401
 
-        file_name = os.path.join(save_path, "region_list.json")
-        assert os.path.exists(file_name)
-        os.remove(file_name)
-    else:
-        assert isinstance(output, pd.DataFrame)
-        assert len(output) == 401
-
-        file_name = os.path.join(save_path, "region_list.csv")
-        assert os.path.exists(file_name)
-        os.remove(file_name)
+    file_name = os.path.join(save_path, "region_list.json")
+    assert os.path.exists(file_name)
+    os.remove(file_name)
