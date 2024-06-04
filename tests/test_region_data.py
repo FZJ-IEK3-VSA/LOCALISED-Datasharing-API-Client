f75057f66358b44ca272b11c7605ab46fe175bb4
s.pati <s.patil@fz-juelich.de>
2024-06-04 08:57:49 +0200

    minor changes

--- tests/test_region_data.py
+++ tests/test_region_data.py
@@ -14,33 +14,45 @@ from zoomin_client import client
 )
 def test_get_region_data(api_key, region_code):
     """Check if region data is returned."""
-    output = client.get_region_data(api_key, country_code="de", region_code=region_code)
-    assert len(output) > 0
-
-
-# TODO: add data value asserts here
-@pytest.mark.parametrize("result_format", ["json", "df"])
-def test_save_region_data(api_key, result_format):
-    """Check if the results of get_region_data() are saved properly."""
     save_path = os.path.join(os.path.dirname(__file__))
-    output = client.get_region_data(
+    output_df = client.get_region_data(
         api_key,
         country_code="de",
-        region_code="DE600_02000000",
-        result_format=result_format,
+        region_code=region_code,
+        result_format="df",
         save_result=True,
         save_path=save_path,
     )
 
-    if result_format == "json":
-        assert isinstance(output, list)
+    # Climate Projection  #TODO
 
-        file_name = os.path.join(save_path, "region_data.json")
-        assert os.path.exists(file_name)
-        os.remove(file_name)
-    else:
-        assert isinstance(output, pd.DataFrame)
+    # Collected var
+    collected_df = output_df[
+        (output_df["var_name"] == "relative_gross_value_added_nace_sector_l")
+    ].copy()
+    assert len(collected_df) == 1
 
-        file_name = os.path.join(save_path, "region_data.csv")
-        assert os.path.exists(file_name)
-        os.remove(file_name)
+    # EUCalc
+    for year in [2020, 2025, 2030, 2035, 2040, 2045, 2050]:
+        for pathway in ["de-lts-st-2050-05062023.json", "de-lts-bc-2050-05062023.json"]:
+            eucalc_df = output_df[
+                (output_df["var_name"].str.startswith("eucalc_"))
+                & (output_df["year"] == year)
+                & (output_df["pathway_file_name"] == pathway)
+            ].copy()
+            assert len(eucalc_df) == 926
+
+    # EUCalc vars assert
+    for var_name in [
+        "eucalc_elc_capex_nuclear",
+        "eucalc_elc_capex_res_solar_pv_utility",
+        "eucalc_bld_capex_reno_off_other",
+        "eucalc_elc_capex_res_other_hydroelectric",
+    ]:
+        eucalc_df = output_df[(output_df["var_name"] == var_name)].copy()
+        assert len(eucalc_df) == 14
+
+    # assert save
+    file_name = os.path.join(save_path, "region_data.csv")
+    assert os.path.exists(file_name)
+    os.remove(file_name)
